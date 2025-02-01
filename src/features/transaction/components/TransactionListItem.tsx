bd681e9a1d799aa1cada4939d8832d2debcd1bda
Shanaka Abeysinghe <shanaka.abeysinghe@axcenze.com>
2025-02-01 19:51:06 +1100

    Basic ui for transaction tracking

---
+++ src/features/transaction/components/TransactionListItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TransactionListItem = () => {
+  return (
+    <div className="bg-slate-50 flex gap-2 justify-between hover:bg-slate-100 p-1 transition-all w-full">
+      <div className="rounded-full bg-black text-white w-14 text-center">T</div>
+      <div>
+        <p className="font-bold text-lg">5421.33</p>
+        <div>from colas</div>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionListItem;
