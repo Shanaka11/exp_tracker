8b5dfad40339dc96f52ecb0ad3a94865d7901cbe
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2025-02-20 12:20:52 +1100

    Fixed summary endpoint

--- src/features/transaction/actions/getTransactionSummaryAction.ts
+++ src/features/transaction/actions/getTransactionSummaryAction.ts
@@ -4,8 +4,7 @@ import { getCurrentUserServer } from '@/features/auth/util/getCurrentUserServer'
 import { getTransactionSummaryUseCase } from '../useCases/transaction/getTransactionSummaryUseCase';
 
 export const getTransactionSummaryAction = async (demo?: boolean) => {
-	console.log(demo);
 	// If boolean, get some dummy data
-	const user = getCurrentUserServer();
+	const user = getCurrentUserServer(demo);
 	return await getTransactionSummaryUseCase(user);
 };
--- src/features/transaction/useCases/transaction/getTransactionSummaryUseCase.ts
+++ src/features/transaction/useCases/transaction/getTransactionSummaryUseCase.ts
@@ -1,6 +1,7 @@
 import { db } from '@/db/drizzle';
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { getSumTransactionAmountService } from '../../services/transaction/getSumTransactionAmountService';
+import { getGoalCostBucketUseCase } from '../costBucket/getGoalCostBucketUseCase';
 
 export const getTransactionSummaryUseCase = async (
 	userId: string,
@@ -14,11 +15,18 @@ export const getTransactionSummaryUseCase = async (
 		connection,
 		`and(eq(user,${userId}),eq(isExpense,false))`
 	);
-	const allocated = 0;
+	const goalCostBucket = await getGoalCostBucketUseCase(connection);
+	if (goalCostBucket.length === 0) {
+		throw new Error('No goal cost bucket found');
+	}
+	const allocated = await getSumTransactionAmountService(
+		connection,
+		`and(eq(user,${userId}),eq(costBucketId,${goalCostBucket[0].id}))`
+	);
 
 	return {
 		totalExpense: parseFloat(expense[0].sum ?? '0'),
 		totalIncome: parseFloat(income[0].sum ?? '0'),
-		allocated: allocated ?? 0,
+		allocated: parseFloat(allocated[0].sum ?? '0'),
 	};
 };
